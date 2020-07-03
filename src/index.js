8b84b1840af080d5b938e25afbd5a9c9585ca74d
Julia Thea <yuliya.boyadjan@gmail.com>
2020-07-02 20:15:51 -0500

    more setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const { send } = require('process')
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup public directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get((''), (req, res) => {
+  res.render('index', {
+    title: 'Your Travel Buddy',
+    name: 'Julia Thea'
+  })
+})
 
 const app = express()
 const port = process.env.PORT || 3000
